f0bbffe293465c3d96831ea333fcca538b2143c0
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-07 16:06:47 -0500

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Noah McGuire on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
